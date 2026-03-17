d0af6ca9bbeb953b3e8261f0cc5e60060c7a68af
Engr.Jimmy <ndumeleikenna@gmail.com>
2026-03-17 23:53:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -115,7 +115,7 @@ resource "aws_security_group" "practice_sg" {
 
 # --- EC2 Practice Instance ---
 resource "aws_instance" "practice_ec2" {
-  ami                    = "ami-0c02fb55956c7d316" # Ubuntu 22.04 in eu-west-1
+  ami                    = "ami = data.aws_ami.ubuntu.id" # Ubuntu 22.04 in eu-west-1
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.practice_public_subnet.id
   security_groups        = [aws_security_group.practice_sg.name]
